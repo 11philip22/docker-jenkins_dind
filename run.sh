ac188017eee08005f5f62a8a854ae5122355fcb4
11philip22 <philipwoldhek@gmail.com>
2019-10-26 15:50:43 +0200

    2019-10-26_15:50:43

--- run.sh
+++
@@ -1,7 +0,0 @@
-# Create Jenkins user
-groupadd -g ${gid} ${group}
-useradd -d ${jenkins_home} -u ${uid} -g ${gid} -m -s /bin/bash ${user}
-chown ${uid}:${gid} /var/jenkins_home
-usermod -aG ${DOCKER_GROUP} ${user}
-USER ${user}
-java -jar /usr/share/jenkins/jenkins.war --httpPort=8090
